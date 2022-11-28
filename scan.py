39d51269521729ab50b0b70bd1fe97485dfc9a5d
shami0306 <rt556639a@gmail.com>
2022-11-28 17:48:46 +0800

    BLE scanner

---
+++ scan.py
@@ -0,0 +1,73 @@
+from bluepy.btle import Scanner, DefaultDelegate
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+from socket import gethostname
+import sys, logging, json, requests
+
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    # def handleDiscovery(self, dev, isNewDev, isNewData):
+    #     if isNewDev:
+    #         print("Discover device {}".format(dev.addr))
+    #     elif isNewData:
+    #         print("Recieved new data from {}".format(dev.addr))
+
+logname = datetime.now().strftime('%Y%m%d')+'.log'
+logger = logging.getLogger('scan')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(logname, maxBytes=5000000, backupCount=99999)
+logger.addHandler(handler)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+#msg = {"type": "note", "title": gethostname(), "body": "BLE Sniffer On"}
+#TOKEN = 'o.nm5hrJ6RbTTpOVPlkgal1k0ByraB8DuI'
+#resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(msg),
+#                headers={'Authorization': 'Bearer ' + TOKEN, 'Content-Type': 'application/json'})
+#if resp.status_code != 200:
+#    raise Exception('Something wrong')
+#else:
+#    print('complete sending')
+
+ble_filter = {
+'0000e6b9-0000-1000-8000-00805f9b34fb',
+'0000ce9c-0000-1000-8000-00805f9b34fb',
+'00002fa9-0000-1000-8000-00805f9b34fb',
+'0000190f-0000-1000-8000-00805f9b34fb',
+'00000f8e-0000-1000-8000-00805f9b34fb',
+'0000efbb-0000-1000-8000-00805f9b34fb',
+'000027ef-0000-1000-8000-00805f9b34fb',
+'00001a05-0000-1000-8000-00805f9b34fb',
+'0000b1fc-0000-1000-8000-00805f9b34fb',
+'0000f22e-0000-1000-8000-00805f9b34fb',
+'0000dd9b-0000-1000-8000-00805f9b34fb',
+'00007dd1-0000-1000-8000-00805f9b34fb',
+'0000ccb5-0000-1000-8000-00805f9b34fb',
+'0000950b-0000-1000-8000-00805f9b34fb',
+'0000fff1-0000-1000-8000-00805f9b34fb'}
+
+
+while True:
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(0.3, passive=True)
+
+    for dev in devices:
+        uid = dev.getValueText(3)
+        if uid == "None":
+            continue
+        logger.info(datetime.now().isoformat()+'\t'+dev.addr+'\t'+dev.addrType+'\t'+str(dev.rssi)+'\t'+dev.getValueText(3))
+        
+        if uid in ble_filter:
+                my_data = {"time":datetime.now().isoformat(),
+                           "mac":dev.addr,
+                           "type":dev.addrType,
+                           "RSSI":str(dev.rssi),
+			   "uuid":uid}
+                r = requests.post('http://140.116.72.66:9527/sniffer_four', data=my_data)
+	# print("Device {} {}, RSSI = {} db {}".format(dev.addr, dev.addrType, dev.rssi, dev.getScanData()))
+        # for (adtype, desc, value) in dev.getScanData():
+        #     print ("{} {}".format(desc, value))
+    
+    
